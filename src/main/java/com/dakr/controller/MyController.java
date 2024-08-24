4dd439901eb21cd48e8d2887be57c3624868e471
Anil Deepa <anil.deepa@wisseninfotech.com>
2024-08-24 12:31:08 +0530

    first commit

--- src/main/java/com/dakr/controller/MyController.java
+++ src/main/java/com/dakr/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.dakr.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ public class MyController {
 	@RequestMapping("/req1")
     public String hi() {
 		return "savePage";
+	}
+	
+	@PostMapping("/save")
+	public String save() {
+		return null;
 	}
 	
 
